00199745cc8805cb612745bc52aaa0f7f5b68967
Aashutosh soni <ashutoshsoni130@gmail.com>
2023-10-22 19:41:23 +0530

    refactor: removed react-google-login package causing dependencie issue.

--- client/chakra-client/src/components/Authentication/Logout.js
+++ client/chakra-client/src/components/Authentication/Logout.js
@@ -1,21 +1,21 @@
-import { GoogleLogout } from "react-google-login";
+// import { GoogleLogout } from "react-google-login";
 
-function onSuccess(setUserData) {
-  alert("Logged out");
-  console.log("Logged Out");
-  setUserData(""); // Clear the user data on logout
-}
+// function onSuccess(setUserData) {
+//   alert("Logged out");
+//   console.log("Logged Out");
+//   setUserData(""); // Clear the user data on logout
+// }
 
-function Logout({ setUserData }) {
-  return (
-    <div id="singOutButton">
-      <GoogleLogout
-        clientId={process.env.REACT_APP_GCP_CLIENT_ID}
-        buttonText="Logout"
-        onLogoutSuccess={() => onSuccess(setUserData)}
-      />
-    </div>
-  );
-}
+// function Logout({ setUserData }) {
+//   return (
+//     <div id="singOutButton">
+//       <GoogleLogout
+//         clientId={process.env.REACT_APP_GCP_CLIENT_ID}
+//         buttonText="Logout"
+//         onLogoutSuccess={() => onSuccess(setUserData)}
+//       />
+//     </div>
+//   );
+// }
 
-export default Logout;
+// export default Logout;
