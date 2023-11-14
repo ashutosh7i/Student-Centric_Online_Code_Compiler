cf52ab2a37c9156c5e8622d1db33a0651210e809
Aashutosh soni <ashutoshsoni130@gmail.com>
2023-11-14 18:50:58 +0530

    Bug fixes🚀🧹

--- client/chakra-client/src/layouts/HomePage.jsx
+++ client/chakra-client/src/layouts/HomePage.jsx
@@ -304,15 +304,18 @@ export default function HomePage() {
 
   useEffect(() => {
     const getUser = () => {
-      fetch("http://localhost:5000/auth/login/success", {
-        method: "GET",
-        credentials: "include",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Credentials": true,
-        },
-      })
+      fetch(
+        "https://soc.centralindia.cloudapp.azure.com/auth/login/success",
+        {
+          method: "GET",
+          credentials: "include",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Credentials": true,
+          },
+        }
+      )
         .then((response) => {
           if (response.status === 200) return response.json();
           throw new Error("authentication has been failed!");
