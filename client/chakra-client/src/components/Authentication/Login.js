ce134708c7b41915436a1ce9c575828ce9fc11a1
Aashutosh soni <ashutoshsoni130@gmail.com>
2023-11-14 18:29:12 +0530

    update: deploying frontend.

--- client/chakra-client/src/components/Authentication/Login.js
+++ client/chakra-client/src/components/Authentication/Login.js
@@ -1,4 +1,7 @@
 export default function Login() {
-  window.open("http://localhost:5000/auth/google", "_self");
-  console.log("Login")
+  window.open(
+    "https://soc.centralindia.cloudapp.azure.com/auth/google",
+    "_self"
+  );
+  console.log("Login");
 }
--- client/chakra-client/src/components/Authentication/Logout.js
+++ client/chakra-client/src/components/Authentication/Logout.js
@@ -1,5 +1,8 @@
 export default function Logout() {
-  window.open("http://localhost:5000/auth/logout", "_self");
+  window.open(
+    "https://soc.centralindia.cloudapp.azure.com/auth/logout",
+    "_self"
+  );
   console.log("Logout");
   alert("You have been logged out");
 }
