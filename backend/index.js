f8b50e398432352d3f1293d383e2cebc8d14e175
Aashutosh soni <ashutoshsoni130@gmail.com>
2023-11-14 18:11:11 +0530

    Update: 1. google auth complete. 2. compilatin done. 3. saveRetrieve done.

---
+++ backend/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cookieSession = require("cookie-session");
+const cors = require("cors");
+const passportSetup = require("./passport");
+const passport = require("passport");
+const authRoute = require("./routes/auth");
+const codeRoute = require("./routes/codeRepo");
+
+app.use(express.json());
+
+// Set up cookie session
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["openreplay"],
+    maxAge: 24 * 60 * 60 * 100,
+  })
+);
+
+// Set up passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+
+// Serving frontend from the same server
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Authentication route
+app.use("/auth", authRoute);
+
+// Code repo route
+app.use("/codedb", codeRoute);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
