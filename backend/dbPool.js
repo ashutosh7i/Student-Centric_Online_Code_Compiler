699b3bcbd8ae89b07331591dda908ee9c58522dc
Aashutosh soni <ashutoshsoni130@gmail.com>
2024-03-05 22:52:43 +0530

    update: Migrated to postgres13.0

--- backend/dbPool.js
+++ backend/dbPool.js
@@ -1,22 +1,19 @@
-const mysql = require("mysql2");
+const { Pool } = require("pg");
 //
 require("dotenv").config();
 
 // Create a MySQL connection pool
-const pool = mysql.createPool({
-  connectionLimit: 10,
+const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  insecureAuth: true,
 });
 
 // check and create table if not exist
 pool.query(
-  // "CREATE TABLE IF NOT EXISTS coderepo (id VARCHAR(255) NOT NULL, filename VARCHAR(255) NOT NULL, timestamp VARCHAR(255) NOT NULL, code VARCHAR(255) NOT NULL, PRIMARY KEY (id, filename))",
-  "CREATE TABLE IF NOT EXISTS coderepo (id varchar(255) NOT NULL, data json DEFAULT NULL, timestamp varchar(45) DEFAULT NULL, filename varchar(45) NOT NULL, PRIMARY KEY (id,filename)  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci",
+  "CREATE TABLE IF NOT EXISTS coderepo (id VARCHAR(255) NOT NULL,data JSONB DEFAULT NULL,timestamp TIMESTAMP DEFAULT NULL,filename VARCHAR(45) NOT NULL,PRIMARY KEY (id, filename))",
 
   (err, res) => {
     if (err) throw err;
